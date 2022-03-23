edc77d341ceac3648b8f886d357bfc60040f5b1b
Selman Altuğ <49366497+muhendisyus@users.noreply.github.com>
2022-03-23 17:12:46 +0300

    Update DefaultController.cs

--- BitirmeProjesi/Controllers/DefaultController.cs
+++ BitirmeProjesi/Controllers/DefaultController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BitirmeProjesi.Models.Sınıflar;
 namespace BitirmeProjesi.Controllers
 {
     public class DefaultController : Controller
     {
+    Context c = ne Context();
         // GET: Default
         public ActionResult Index()
         {
+            var dgr = c.tYTMatKonulari.ToList();
             return View();
         }
     }
