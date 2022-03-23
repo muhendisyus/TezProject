36f4003270e16873b91ffbd84f291a760bc76241
FatihKURT61 <102238058+FatihKURT61@users.noreply.github.com>
2022-03-23 18:35:20 +0300

    Create AboutController.cs

---
+++ BitirmeProjesi/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace BitirmeProjesi.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+       
+        public ActionResult Index()
+        {
+            
+            return View(degerler);
+        }
+    }
+}
